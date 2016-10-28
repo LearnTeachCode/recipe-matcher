95786aa7e76e7bad5c1c5c25cf97ec121e30949f
bean00 <jcbuilder40@hotmail.com>
2016-10-27 19:48:25 -0700

    Add import statements

--- src/main/java/devApp/entity/recipe/model/Recipe.java
+++ src/main/java/devApp/entity/recipe/model/Recipe.java
@@ -6,7 +6,9 @@ import java.util.List;
 import java.util.Set;
 
 import javax.persistence.CascadeType;
+import javax.persistence.CollectionTable;
 import javax.persistence.Column;
+import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
